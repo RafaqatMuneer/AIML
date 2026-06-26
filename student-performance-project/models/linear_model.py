f3da5a8c5e7c244ed9389d97685fa29dff08185c
RafaqatMuneer <rafaqatmunir@gmail.com>
2026-06-27 04:06:46 +0500

    pkl file created

--- student-performance-project/models/linear_model.py
+++ student-performance-project/models/linear_model.py
@@ -13,3 +13,5 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 model = LinearRegression()
 model.fit(X_train, y_train)
 
+with open("models/linear_model.pkl", "wb") as f:
+    pickle.dump(model,f)
